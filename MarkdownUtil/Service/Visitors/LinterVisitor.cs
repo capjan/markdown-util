d105cf25305258f98ece06bedfe2fd55e953217a
Jan Ruhlaender <jan.ruhlaender@gmail.com>
2022-02-05 23:43:16 +0100

    fix: fixed a typo

--- MarkdownUtil/Service/Visitors/LinterVisitor.cs
+++ MarkdownUtil/Service/Visitors/LinterVisitor.cs
@@ -17,7 +17,7 @@ public class LinterVisitor: IVisitor<MarkdownFile>
     {
         if (string.IsNullOrWhiteSpace(entity.Title))
         {
-            _errorReceiver.Add(entity.FileInfo.FullName, 1, "Page must start with a headline 1 that defines it Title. Add # pagetitle to the first line.ß");
+            _errorReceiver.Add(entity.FileInfo.FullName, 1, "Page must start with a headline 1 that defines its title. Add a '# pagetitle' to the first line of the document");
         }
 
         return true;
