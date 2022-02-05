8db7301f48ae3f2883eb2b474272507d2d2fabe0
Jan Ruhlaender <jan.ruhlaender@gmail.com>
2022-02-05 23:44:59 +0100

    fix: BreadcrumbNavigation won't be detected on root pages

--- MarkdownDocument/Impl/RegExPattern.cs
+++ MarkdownDocument/Impl/RegExPattern.cs
@@ -6,5 +6,5 @@ internal static class RegExPattern
     /// Matches any Markdown Header. Groups the Optional Title and Breadcrumbs Navigation line
     /// </summary>
     public const string MarkdownHeader =
-        @"^(?:#\s+(?<titleName>[\w\s-[\n]]+)(?=\r?\n))?(?:\s*(?:\r?\n))*(?<breadcrumbs>\[.*)?(?:\s*(?:\r?\n))*(?=.)";
+        @"^(?:[\s\n\r]*#\s+(?<titleName>.+))?(?:\s*(?:\r?\n))*(?<breadcrumbs>(?:\[.*|\k<titleName>))?(?:\s*(?:\r?\n))*(?=.)?";
 }
