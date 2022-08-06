8b505db05c208654e11554d94b496d62f2d8b23d
Jan Ruhlaender <jan.ruhlaender@gmail.com>
2022-08-06 15:39:09 +0200

    test: fixed unit test

--- MarkdownUtilTest/UnitTest1.cs
+++ MarkdownUtilTest/UnitTest1.cs
@@ -4,10 +4,10 @@ public class UnitTest1
 {
     [Theory]
     [InlineData("/folder1/folder2/folder3/", "/folder1/folder2/folder3/folder4/", "folder4/")]
-    [InlineData("/folder1/folder2/folder3/", "/folder1/folder2/folder3/folder4", "folder4/")]
+    [InlineData("/folder1/folder2/folder3/", "/folder1/folder2/folder3/folder4", "folder4")]
     public void Test1(string origin, string target, string expected)
     {
-        var result = System.IO.Path.GetRelativePath(origin, target);
+        var result = Path.GetRelativePath(origin, target);
         Assert.Equal(expected, result);
     }
 }
